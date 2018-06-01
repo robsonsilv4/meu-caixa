a5ab1d72d2f889ee12c8a7d744cd1e1d71154ee5
robsonsilv4 <robsonsilv410@gmail.com>
2018-06-01 12:41:54 -0300

    Templates, base e histórico

--- venda/urls.py
+++ venda/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 
-path('historico', views.historico, name='historico'),
+urlpatterns = [
+    path('historico', views.historico, name='historico'),
+]
