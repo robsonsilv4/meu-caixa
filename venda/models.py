d92e47cdfb621c4a0079995e92a9eea120c4e5b7
robsonsilv4 <robsonsilv410@gmail.com>
2018-06-01 12:07:47 -0300

    Model de venda e registro no admin

--- venda/models.py
+++ venda/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Venda(models.Model):
+    data = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField()
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def adicionar(self):
+        self.data = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.descricao
